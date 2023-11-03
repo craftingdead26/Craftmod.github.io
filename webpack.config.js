f5db1dace3c80d7fd0fbbb0c18345978eeefb0f3
Gabs <140965985+GabsTheCuriousKid@users.noreply.github.com>
2023-11-03 10:52:28 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -167,35 +167,35 @@ module.exports = [
                 chunks: ['editor'],
                 template: 'src/playground/index.ejs',
                 filename: 'editor.html',
-                title: 'PenguinMod - A mod of TurboWarp',
+                title: 'DinosaurMod - A mod of Penguinmod',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['playground'],
                 template: 'src/playground/index.ejs',
                 filename: 'playground.html',
-                title: 'PenguinMod - A mod of TurboWarp',
+                title: 'DinosaurMod - A mod of Penguinmod',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['player'],
                 template: 'src/playground/index.ejs',
                 filename: 'index.html',
-                title: 'PenguinMod - A mod of TurboWarp',
+                title: 'DinosaurMod - A mod of Penguinmod',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['fullscreen'],
                 template: 'src/playground/index.ejs',
                 filename: 'fullscreen.html',
-                title: 'PenguinMod - A mod of TurboWarp',
+                title: 'DinosaurMod - A mod of Penguinmod',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['embed'],
                 template: 'src/playground/index.ejs',
                 filename: 'embed.html',
-                title: 'Embedded Project - PenguinMod',
+                title: 'Embedded Project - DinosaurMod',
                 noTheme: true,
                 ...htmlWebpackPluginCommon
             }),
@@ -203,14 +203,14 @@ module.exports = [
                 chunks: ['addon-settings'],
                 template: 'src/playground/simple.ejs',
                 filename: 'addons.html',
-                title: 'Addon Settings - PenguinMod',
+                title: 'Addon Settings - Dinosaurmod',
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['credits'],
                 template: 'src/playground/simple.ejs',
                 filename: 'credits.html',
-                title: 'PenguinMod & TurboWarp Credits',
+                title: 'DinosaurMod, PenguinMod & TurboWarp Credits',
                 noSplash: true,
                 ...htmlWebpackPluginCommon
             }),
