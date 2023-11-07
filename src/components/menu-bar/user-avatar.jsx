e8d719c250cd870110987616c3d7416cbef2f958
craftingdead26 <106111718+craftingdead26@users.noreply.github.com>
2023-11-07 12:49:53 -0600

    Update user-avatar.jsx

--- src/components/menu-bar/user-avatar.jsx
+++ src/components/menu-bar/user-avatar.jsx
@@ -9,7 +9,7 @@ const UserAvatar = ({
     imageUrl,
     username
 }) => (
-    <a target="_blank" href={"https://penguinmod.com/profile?user=" + username}>
+    <a target="_blank" href={"https://pd.com/profile?user=" + username}>
         <img
             className={classNames(
                 className,
